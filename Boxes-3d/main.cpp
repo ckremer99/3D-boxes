40249c0502211898a26c64605c65fab6a9ce6ff7
ckremer99 <ckremer99@outlook.com>
2024-10-03 11:00:10 -0700

    Initial Commit

---
+++ Boxes-3d/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Boxes-3d
+//
+//  Created by Cody Kremer on 10/3/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
